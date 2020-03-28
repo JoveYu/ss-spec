4fe361997d1ffd64fe5a6d1b3276589776c10dcf
Jove Yu <yushijun110@126.com>
2020-03-28 10:55:09 +0800

    update

---
+++ iptools/gen.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+curl 'http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest' | grep ipv4 | grep CN | awk -F\| '{ printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > chnroute.txt
+cat private.txt chnroute.txt | python negate_network.py > foreign.txt
